2cbd1c73107bf4a86215840f3d907d94e4b79b0b
raghavendra <raghavendra0360@gmail.com>
2026-02-02 13:57:36 +0530

    Maps and 2d arrays

--- go-tour/phase-3/datastructures.go
+++ go-tour/phase-3/datastructures.go
@@ -32,6 +32,11 @@ func main() {
 	//Note slices are references to array not new arrays per se
 	names := [5]string{"ina", "mina", "dika", "amar", "akbar"}
 
+	/* We can append to slices
+	However it creates new allocation on overflow
+	so need to check if we can experience performance issues
+	*/
+
 	fmt.Println(names)
 	c := names[0:2]
 	d := names[2:5]
@@ -50,4 +55,41 @@ func main() {
 		{3, false},
 	}
 	fmt.Println(s)
+
+	rangeOnArrays()
+
+	// pic.Show(Pic)
+	maps()
+
+}
+
+func rangeOnArrays() {
+	var pow = []int{1, 2, 4, 8, 16}
+	for i := range pow {
+		fmt.Println(i)
+	}
+}
+
+func Pic(dx, dy int) [][]uint8 {
+	pic := make([][]uint8, dy)
+
+	for i := range pic {
+		pic[i] = make([]uint8, dx)
+		for j := range pic[i] {
+			pic[i][j] = uint8(i*i + j*j)
+		}
+	}
+	return pic
+}
+
+type Vertex struct {
+	Lat, Long float64
+}
+
+var m map[string]Vertex
+
+func maps() {
+	m = make(map[string]Vertex)
+	m["Bell Labs"] = Vertex{1, 5}
+	fmt.Println(m)
 }
