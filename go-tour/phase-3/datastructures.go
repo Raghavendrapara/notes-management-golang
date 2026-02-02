f6de6637278d730fc5241c7183c1d1d64f56fe3b
raghavendra <raghavendra0360@gmail.com>
2026-02-02 12:02:06 +0530

    Memory leak prospects due to slicing

--- go-tour/phase-3/datastructures.go
+++ go-tour/phase-3/datastructures.go
@@ -4,6 +4,22 @@ import (
 	"fmt"
 )
 
+/*
+The "Memory Leak" Risk: This is actually a common production issue. If you load a 100MB file into a slice fileData, and then do:
+
+Go
+header := fileData[:100] // Take first 100 bytes
+return header
+If you return header and keep using it, the entire 100MB array stays in RAM, because header is still pointing to the start of that huge block.
+
+The Fix (if you actually wanted to free memory): You must copy the data to a new, independent slice.
+
+Go
+// Create a brand new independent slice
+newS := make([]int, len(s))
+copy(newS, s)
+// Now the old backing array can be garbage collecte
+*/
 func main() {
 	var a [2]string
 	a[0] = "abv"
@@ -24,4 +40,14 @@ func main() {
 	fmt.Println(c)
 	fmt.Println(names)
 
+	//struct arrays
+
+	s := []struct {
+		i int
+		b bool
+	}{
+		{2, true},
+		{3, false},
+	}
+	fmt.Println(s)
 }
