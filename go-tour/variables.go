7189d0f3f8776803fb300f77c9179590d31b7de8
raghavendra <raghavendra0360@gmail.com>
2026-02-02 10:34:22 +0530

    phase 1:

--- go-tour/variables.go
+++ go-tour/variables.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"math/cmplx"
+	"runtime"
+	"time"
 )
 
 var d, e bool
@@ -11,7 +14,49 @@ func main() {
 	b := 10
 	fmt.Println(a, b, d, e)
 	fmt.Println(add(a, b))
+	fmt.Println(time.Date(2026, 2, 2, 10, 0, 0, 0, time.UTC))
+	shorthand()
+	variables()
+	loops()
+	runtimes()
 }
 func add(a, b int) (x int) {
 	return a + b
+}
+func shorthand() {
+	i := 10
+	fmt.Println(i)
+}
+
+func variables() {
+	z := cmplx.Sqrt(-5)
+	max := uint64(1) << 63
+
+	fmt.Println(z, max)
+
+	fmt.Printf("Type: %T Value: %v\n", max, max)
+
+}
+
+func loops() {
+	for i := 0; i < 10; i++ {
+		// fmt.Println(i)
+	}
+	i := 0
+	for i < 5 {
+		i++
+		// fmt.Println(i)
+	}
+}
+func runtimes() {
+	fmt.Printf("Running on ")
+
+	switch runtime.GOOS {
+	case "darwin":
+		fmt.Println("OS X.")
+	case "linux":
+		fmt.Println("Linux.")
+	default:
+		fmt.Printf("OS: %s.\n", runtime.GOOS)
+	}
 }
