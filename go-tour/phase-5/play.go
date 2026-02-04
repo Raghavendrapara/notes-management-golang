1dd5da98e460d949e05fcd9d06899772cecdf36a
raghavendra <raghavendra0360@gmail.com>
2026-02-04 18:30:47 +0530

    switch case no break, fallthrough is forced not default

--- go-tour/phase-5/play.go
+++ go-tour/phase-5/play.go
@@ -1,14 +1,19 @@
 package main
 
-import "fmt"
+import ("fmt"
+ "runtime")
 
 func main() {
 	users := map[string]string{"admin": "root"}
 	tryToReplace(users)
 
 	fmt.Printf("Address of map m: %p\n", &users)
-	fmt.Println(*&users)
+	fmt.Println("main ", *&users)
 	addressPrint(&users)
+	fmt.Println("main ", users)
+	fmt.Println(runtime.GOOS)
+
+	switchcase("b")
 }
 
 func tryToReplace(m map[string]string) {
@@ -19,4 +24,20 @@ func tryToReplace(m map[string]string) {
 func addressPrint(ptr *map[string]string) {
 	fmt.Println(&ptr)
 	fmt.Println(*ptr)
+
+	// *ptr = map[string]string{"n":"n"}
+	// ptr = &map[string]string{"m": "n"}
+	// fmt.Println(*ptr)
+
+	*ptr = nil
+}
+
+/* 
+Notice there is no need for break like java
+*/
+func switchcase(a string){
+	switch a{
+	case "a" : fmt.Println("A")
+	case "b" : fmt.Println("B")
+	}
 }
